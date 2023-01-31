055398ac7b29f845275fe1a51803fe3be31651b5
Martin van Amersfoorth <martin.van.amersfoorth@finalist.nl>
2023-01-31 15:29:53 +0100

    Add revalidate API route with token validation

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -5,6 +5,7 @@ module.exports = {
   },
   env: {
     STRAPI_API_TOKEN: process.env.STRAPI_API_TOKEN,
-    STRAPI_API_URL: process.env.STRAPI_API_URL
+    STRAPI_API_URL: process.env.STRAPI_API_URL,
+    REVALIDATE_TOKEN: process.env.REVALIDATE_TOKEN
   }
 };
--- apps/web/pages/api/revalidate.ts
+++ apps/web/pages/api/revalidate.ts
@@ -1,7 +1,14 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function handler( req: NextApiRequest, res: NextApiResponse ) {
+
+    if ( req.query.token !== process.env.REVALIDATE_TOKEN ) {
+        return res.status(401).json({ message: 'invalid token'} )
+    }
+
     try {
+        console.log('revalidate - body:', req.body)
+        await res.revalidate(`/posts/${req.body?.entry?.slug}`)
         await res.revalidate('/posts')
         return res.json( {revalidated: true} )
     } catch (err ) {
