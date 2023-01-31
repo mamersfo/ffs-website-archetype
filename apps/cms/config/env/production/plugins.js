92cfa11498dff59a29279fadc5ff45e13b3deced
Martin van Amersfoorth <martin.van.amersfoorth@finalist.nl>
2023-01-31 22:39:18 +0100

    More cloudinary

--- apps/cms/config/env/production/plugins.js
+++ apps/cms/config/env/production/plugins.js
@@ -7,6 +7,11 @@ module.exports = ({ env }) => ({
         api_key: env("CLOUDINARY_KEY"),
         api_secret: env("CLOUDINARY_SECRET"),
       },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
     },
   },
 });
